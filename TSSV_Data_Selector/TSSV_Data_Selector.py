20c8889f6352c444b0cfd8c0d45bbb235d279c9d
OpenPMU Technologies <31275182+OpenPMU@users.noreply.github.com>
2024-08-13 12:13:07 +0100

    Add files via upload

--- TSSV_Data_Selector/TSSV_Data_Selector.py
+++ TSSV_Data_Selector/TSSV_Data_Selector.py
@@ -40,25 +40,25 @@ from matplotlib.ticker import FuncFormatter, AutoLocator, AutoMinorLocator, Mult
 def select_file_from_timestamp(time_in, extension='.flac', file_interval=5):
     
     # Round down start time to the nearest 5 minutes
-    file_minute = start_time.minute - (start_time.minute % file_interval)
-    file_time = start_time.replace(minute=file_minute, second=0, microsecond=0)
+    file_minute = time_in.minute - (time_in.minute % file_interval)
+    file_time = time_in.replace(minute=file_minute, second=0, microsecond=0)
     
     # Convert start time to filename
     filename = file_time.strftime("%Y-%m-%d_%H-%M-%S") + extension
     
     return filename
 
-def extract_audio_section(start_time, duration_ms, file_interval=5):
+def extract_audio_section(start_time, duration_ms, file_interval=5, directory=''):
 
     # Open the first audio file
-    audio_file = sf.SoundFile(select_file_from_timestamp(start_time))
+    audio_file = sf.SoundFile(directory + select_file_from_timestamp(start_time))
     
     # Calculate the start position in samples
     start_seconds = (start_time.minute % file_interval)*60 + start_time.second + start_time.microsecond / 1000000
     start_sample = int(start_seconds * audio_file.samplerate)
     
-    print(start_seconds)
-    print(start_sample)
+    # print(start_seconds)
+    # print(start_sample)
     
     # Seek to the start position in the file
     audio_file.seek(start_sample)
@@ -112,63 +112,63 @@ def format_time(x, pos=None):
     # return f"{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}"
     return f"{seconds:02d}.{milliseconds:03d}"
 
+if __name__ == "__main__":
+    # Example usage
+    start_time = datetime(2024, 3, 17, 9, 4, 56, microsecond=450000)
+    duration_ms = 200  # duration in milliseconds
 
-# Example usage
-start_time = datetime(2024, 3, 17, 9, 4, 56, microsecond=450000)
-duration_ms = 200  # duration in milliseconds
-
-TSSV_data, samplerate = extract_audio_section(start_time, duration_ms)
-# print("TSSV data:", TSSV_data)
-# print("Sample rate:", samplerate)
-
-
+    TSSV_data, samplerate = extract_audio_section(start_time, duration_ms)
+    # print("TSSV data:", TSSV_data)
+    # print("Sample rate:", samplerate)
 
 
-# Generate time axis in milliseconds
-time_ms = np.arange(len(TSSV_data)) / samplerate * 1000
-
-time_ms += start_time.microsecond / 1000
-
-# Plot the audio data
-plt.figure(figsize=(10, 4))
-plt.plot(time_ms, TSSV_data)
-plt.xlabel(f"Time (ms) - (Start Time: {start_time.strftime('%Y-%m-%d %H:%M:%S.%f')} UTC)")
 
 
-# Customize the time axis labels
+    # Generate time axis in milliseconds
+    time_ms = np.arange(len(TSSV_data)) / samplerate * 1000
 
-# plt.gca().xaxis.set_major_locator(AutoLocator())
-# plt.gca().xaxis.set_minor_locator(AutoMinorLocator())
+    time_ms += start_time.microsecond / 1000
 
-plt.ylabel("Amplitude")
-plt.grid(True)
-
-# Enable minor gridlines
-plt.minorticks_on()
-plt.grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
-
-# Add vertical gridlines at every 20 ms interval
-base_frequency = 50                         # Nomiinal system frequency in Hertz
-base_period = (1 / base_frequency) * 1000   # Period in ms
-first_synchrotick = int(((start_time.microsecond / 1000) // base_period ) * base_period)
-for t in range(first_synchrotick, int(max(time_ms)), 20):
-    plt.axvline(x=t, color='green', linestyle='--', linewidth=0.5)
+    # Plot the audio data
+    plt.figure(figsize=(10, 4))
+    plt.plot(time_ms, TSSV_data)
+    plt.xlabel(f"Time (ms) - (Start Time: {start_time.strftime('%Y-%m-%d %H:%M:%S.%f')} UTC)")
 
 
-# Customize the time axis ticks
-major_locator = MultipleLocator(20)  # Preferred tick locations at 20 ms intervals
-major_locator = AutoLocator()
-plt.gca().xaxis.set_major_locator(major_locator)
-plt.gca().xaxis.set_minor_locator(AutoMinorLocator())
-plt.gca().xaxis.set_major_formatter(lambda x, pos: f"{x:.0f}")  # Format tick labels as integers
-plt.gca().xaxis.set_major_formatter(FuncFormatter(format_time))
+    # Customize the time axis labels
+
+    # plt.gca().xaxis.set_major_locator(AutoLocator())
+    # plt.gca().xaxis.set_minor_locator(AutoMinorLocator())
+
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+
+    # Enable minor gridlines
+    plt.minorticks_on()
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
+
+    # Add vertical gridlines at every 20 ms interval
+    base_frequency = 50                         # Nomiinal system frequency in Hertz
+    base_period = (1 / base_frequency) * 1000   # Period in ms
+    first_synchrotick = int(((start_time.microsecond / 1000) // base_period ) * base_period)
+    for t in range(first_synchrotick, int(max(time_ms)), 20):
+        plt.axvline(x=t, color='green', linestyle='--', linewidth=0.5)
 
 
-# Use AutoLocator and AutoMinorLocator for minor ticks
+    # Customize the time axis ticks
+    major_locator = MultipleLocator(20)  # Preferred tick locations at 20 ms intervals
+    major_locator = AutoLocator()
+    plt.gca().xaxis.set_major_locator(major_locator)
+    plt.gca().xaxis.set_minor_locator(AutoMinorLocator())
+    plt.gca().xaxis.set_major_formatter(lambda x, pos: f"{x:.0f}")  # Format tick labels as integers
+    plt.gca().xaxis.set_major_formatter(FuncFormatter(format_time))
 
-plt.gca().yaxis.set_minor_locator(AutoMinorLocator())
+
+    # Use AutoLocator and AutoMinorLocator for minor ticks
+
+    plt.gca().yaxis.set_minor_locator(AutoMinorLocator())
 
 
-plt.show()
+    plt.show()
 
 
